989687fd81a10d9eaa6821e27441f9ddba3faee3
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-07 00:00:00 +0100

    fixed connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './App.scss';
-import io from 'socket.io-client';
+import * as io from 'socket.io-client';
 import Chat from './components/Chat';
 
-const socket = io('http://localhost::3620');
+const socket = io.connect('http://localhost::3620');
 
 function App() {
 	const [username, setUsername] = useState('');
